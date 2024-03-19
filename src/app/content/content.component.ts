ddceb8ccbeb4a870ebc5bc1d30936bbb8ba1965f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-19 20:17:41 -0300

    feat: importados os dados do JSON que mocka o retorno da API

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from "./movie-card/movie-card.component";
+import * as moviesData from "../../assets/mock_omdb.json"
 
 @Component({
     selector: 'app-content',
     standalone: true,
     templateUrl: './content.component.html',
-    imports: [MovieCardComponent]
+    imports: [CommonModule, MovieCardComponent]
 })
 export class ContentComponent {
+    private movies: any = moviesData;
 
+    public getMovies() {
+        return this.movies.Search;
+    }
 }
