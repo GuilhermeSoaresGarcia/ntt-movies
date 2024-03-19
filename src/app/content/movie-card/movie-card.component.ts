2da810e370f5e9218d0f6f3b3521d8ba6ece03a4
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-19 20:19:56 -0300

    feat: definida a tipagem do atributo movie com a interface MovieCard

--- src/app/content/movie-card/movie-card.component.ts
+++ src/app/content/movie-card/movie-card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MovieCard } from '../interfaces/MovieCard';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
   imports: [],
-  templateUrl: './movie-card.component.html'  
+  templateUrl: './movie-card.component.html'
 })
 export class MovieCardComponent {
-
+  @Input() public movie!: MovieCard;
 }
