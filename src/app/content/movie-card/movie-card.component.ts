9e2c7ec9285d6136224c0858a555e4a9260dd283
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-19 18:55:49 -0300

    feat: criado component do movie-card

---
+++ src/app/content/movie-card/movie-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-card.component.html'  
+})
+export class MovieCardComponent {
+
+}
