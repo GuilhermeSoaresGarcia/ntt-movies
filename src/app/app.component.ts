32580c28a48456abc29e1679d8260c65bc700561
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-21 16:00:26 -0300

    feat: implementado o uso do router do Angular para o uso de rotas dinâmicas dos detalhes dos filmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { ContentComponent } from "./content/content.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',    
-    imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent]  
 })
 export class AppComponent {
   title = 'ntt-movies';
- }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { MovieCardDetailsComponent } from './content/movie-card/movie-card-details/movie-card-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ContentComponent },
+  { path: "details/:id", component: MovieCardDetailsComponent }
+];
