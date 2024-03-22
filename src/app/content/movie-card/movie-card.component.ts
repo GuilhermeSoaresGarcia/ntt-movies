307e627b00926740eaa7e308eaa32a9d7ad9c48d
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-21 21:25:48 -0300

    feat: implementado botão de favorito com toggle em MovieCardComponent

--- src/app/content/movie-card/movie-card.component.ts
+++ src/app/content/movie-card/movie-card.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MovieCard } from '../../interfaces/MovieCard';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie-card.component.html'
 })
 export class MovieCardComponent {
   @Input() public movie!: MovieCard;
+  isFavorite = false;
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+    console.log(this.isFavorite)
+  }
 }
