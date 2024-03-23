897401be62e8ec86835abf4af5dc14a138c7c24f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-23 14:07:21 -0300

    feat: implementado placeholder para o loading das cards de informações dos filmes

--- src/app/content/movie-card/movie-card-details/movie-card-details.component.ts
+++ src/app/content/movie-card/movie-card-details/movie-card-details.component.ts
@@ -1,34 +1,50 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MovieCard } from '../../../interfaces/MovieCard';
 import { ActivatedRoute } from '@angular/router';
 import { ApiFetchService } from '../../../services/apifetch.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-card-details',
   standalone: true,
-  imports: [HttpClientModule],
-  providers: [ApiFetchService],
+  imports: [CommonModule, HttpClientModule],
+  providers: [ApiFetchService, Location],
   templateUrl: './movie-card-details.component.html',
 })
-export class MovieCardDetailsComponent implements OnInit {
-  // FONTE: https://angular.io/tutorial/tour-of-heroes/toh-pt5#routable-herodetailcomponent
-  public movieDetails!: MovieCard;
 
-  // Bindings no construtor para o serviço de captura de rota (do próprio Angular) e do serviço
-  // que criei para fazer as requisições na API
-  constructor(private route: ActivatedRoute, private apiFetchService: ApiFetchService) { }
+export class MovieCardDetailsComponent implements OnInit, OnDestroy {
+  public movieDetails?: MovieCard;
+  public isLoading: boolean = true;
+  private subscription: Subscription = new Subscription();
+
+  constructor(private route: ActivatedRoute,
+    private apiFetchService: ApiFetchService,
+    private location: Location) { }
 
   ngOnInit(): void {
     this.getMovieInfo();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   getMovieInfo() {
-    // Pega o id da url
+    // FONTE: https://angular.io/tutorial/tour-of-heroes/toh-pt5#routable-herodetailcomponent
     const id = this.route.snapshot.paramMap.get('id');
 
-    // Utiliza o id capturado para fazer a requisição na api utilizando o serviço criado para isso
-    this.apiFetchService.getMovieById(id!)
-      .subscribe(movie => this.movieDetails = movie as MovieCard)
+    this.subscription = this.apiFetchService.getMovieById(id!)
+      .subscribe(movie => this.movieDetails = movie as MovieCard);
+
+    // Coloquei um pequeno intervalo porque embora a requisição da API demore cerca de 1/2 segundo,
+    // o template de loading não estava entrando
+    setInterval(() => this.isLoading = false, 700)
+  }
+
+  getBackToWhereYouOnceBelonged(): void {
+    this.location.back();
   }
 }
