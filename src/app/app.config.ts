4a0330384ea215c0958caac88336f2ab38065e28
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-24 18:21:06 -0300

    feat: incluído o provideStoreDevtools no providers do appConfig para que o redux-dev-tools funcione com a aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { reducers } from './reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(reducers)]
+  providers: [
+    provideRouter(routes),
+    provideStore(reducers),
+    provideStoreDevtools({ maxAge: 25 })
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducers, metaReducers } from './app/reducers';
 
-bootstrapApplication(AppComponent, appConfig)
-  .then(() => {
-    StoreModule.forRoot(reducers, { metaReducers }),
-      StoreDevtoolsModule.instrument({
-        name: 'NTT Movie',
-        logOnly: true
-      });
-  }).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
