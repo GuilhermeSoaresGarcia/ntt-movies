bb32e15eedbeea100b15d46d25fef12a3216c192
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-24 11:19:02 -0300

    refactor: implementa lógica para mostrar alguns títulos aleatórios na abertura do app

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener, OnDestroy, inject } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from "./movie-card/movie-card.component";
 import { ApiFetchService } from '../services/apifetch.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MovieCard } from '../interfaces/MovieCard';
 import { FormsModule } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, defaultIfEmpty, tap } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 
 @Component({
@@ -27,19 +27,22 @@ export class ContentComponent implements OnDestroy {
         this.searchTerm$ = this.store.pipe(select('search', 'searchResults'))
     }
 
-
     ngOnInit(): void {
+        const loadingInterval = 700;
         this.isLoading = true;
-        if (this.searchTerm$ == null) {
-            this.fetchRandomMovies()
-        } else {
-            this.subscription = this.searchTerm$.subscribe((movies) => {
-                this.searchResult$ = movies
-            });
-        }
-        this.isLoading = false;
-    }
 
+        this.subscription = this.searchTerm$.pipe(
+            defaultIfEmpty([]), // Check if the observable is empty
+            tap(movies => {
+                if (movies == undefined) { // A sugestão do ChatGPT foi outra, mas estava resultando undefined pra mim então usei isso como condicional
+                    this.fetchRandomMovies(); // Fetch random movies if the observable is empty (neste caso, não "empty" mas "undefined")
+                    setInterval(() => { this.isLoading = false }, loadingInterval)
+                } else {
+                    this.searchResult$ = movies; // Set search results if the observable has data
+                    setInterval(() => { this.isLoading = false }, loadingInterval)
+                }
+            })).subscribe();
+    }
 
     ngOnDestroy(): void {
         this.subscription.unsubscribe()
